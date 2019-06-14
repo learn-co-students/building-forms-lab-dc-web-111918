db4591306d673ed45895a515c9c66a0cfc17d4b7
rykyou <rkim1214@gmail.com>
2019-06-14 14:17:41 -0400

    Finished.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,43 @@
-// Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ''
+    });
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <h3>Band Input</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add Band</label>
+          <input type="text" onChange={this.handleChange} value={this.state.name}/>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
-export default BandInput
+const mapDispatchToProps = dispatch => {
+  return {
+    addBand: (bandData) => dispatch({type: 'ADD_BAND', payload: bandData})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(BandInput);
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput';
+import Band from '../components/Band';
 
 class BandsContainer extends Component {
+  renderAllBands = () => this.props.bands.map((band, idx) => <Band key={idx} name={band}/>)
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
+        <div>
+          <h3>All Bands:</h3>
+          <div>{this.renderAllBands()}</div>
+        </div>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+export default connect(mapStateToProps)(BandsContainer);
